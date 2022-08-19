204318c9376ed310ad4d58f802d48ede64744337
Vishal-Techy <vishalchinns1650@gmail.com>
2022-08-19 16:35:09 +0530

    started manual control page

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import '../styles/globals.css'
 import '../styles/dashboard.module.css'
 import '../styles/robotHealth.css'
-// import '../styles/farmingMode.css'
-// import '../styles/manualControl.css'
+import '../styles/farmingMode.css'
+import '../styles/manualControl.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
